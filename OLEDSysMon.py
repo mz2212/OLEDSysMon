f7a26b2ea9965e5357ef75275225591a4f3007c9
Mike <michaelpulliam1@gmail.com>
2018-02-06 22:54:45 -0700

    Found a better way
    I wish that this was quite a bit more generic, but it's a pita to send more than a couple bytes at once. Guess I could have the Arduino ask for the data.

--- OLEDSysMon.py
+++ OLEDSysMon.py
@@ -8,13 +8,23 @@ ser = serial.Serial(port, baud)
 
 try:
 	while 1:
+		# Prepare for writing.
+		data = ord('B').to_bytes(1, byteorder="big")
+		ser.write(data) 
 		# Time bytes
+		data = ord('C').to_bytes(1, byteorder="big")
+		ser.write(data)
 		data = int(time.strftime("%I")).to_bytes(1, byteorder="big")
 		ser.write(data)
 		data = int(time.strftime("%M")).to_bytes(1, byteorder="big")
 		ser.write(data)
 		# CPU Usage
+		data = ord('D').to_bytes(1, byteorder="big")
+		ser.write(data)
 		data = int(psutil.cpu_percent(interval=0.5)).to_bytes(1, byteorder="big")
+		ser.write(data)
+		# Flush the buffers
+		data = ord('Z').to_bytes(1, byteorder="big")
 		ser.write(data)
 		time.sleep(0.1)
 
--- oledsysmon.ino
+++ oledsysmon.ino
@@ -4,9 +4,8 @@
 
 Adafruit_SSD1306 display(4);
 
-int data = 0;
-char buff[4] = "";
-String toPrint = "";
+char mode, d1, d2;
+char buff[16] = "";
 
 void setup() {
 	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -20,26 +19,38 @@ void setup() {
 
 void loop() {
 	if(Serial.available() > 0) {
-		// This is really clumsy, but it's the best I can figure...
-		// The limits are basically what fit in a byte,
-		// though with a bit of cleverness, you can get around that.
-		//Time/Countdown... Somthing like that.
-		display.setCursor(0, 0);
-		display.clearDisplay();
-		data = Serial.read();
-		sprintf(buff, "%02d", data);
-		display.print(buff); display.print(":");
-		while(Serial.available() == 0) {}
-		data = Serial.read();
-		sprintf(buff, "%02d", data);
-		display.println(buff);
-		//CPU Usage
-		while(Serial.available() == 0) {}
-		data = Serial.read();
-		display.print("CPU:");
-		display.print(data);
-		display.println("%");
-		
-		display.display();
+		// So I guess I figured a better way of handling this.
+		// A multi-byte protocol is how I'm going to handle it now.
+		// I.E. 'C''10''24' to set the clock/timer string.
+		// I might add a cursor positioning thingy later.
+		mode = Serial.read();
+		if(mode == 'A') { // Blank the display.
+			display.setCursor(0, 0);
+			display.clearDisplay();
+			display.display();
+		}
+		if(mode == 'B') { // Prepare for writing.
+			display.setCursor(0, 0);
+			display.clearDisplay();
+		}
+		if(mode == 'C') { // Set time string
+			display.setCursor(0, 0);
+			while(Serial.available() == 0) {}
+			d1 = Serial.read();
+			while(Serial.available() == 0) {}
+			d2 = Serial.read();
+			sprintf(buff, "%02d:%02d", d1, d2);
+			display.print(buff);
+		}
+		if(mode == 'D') { // Set CPU String
+			display.setCursor(0, 16); // Characters are 16px tall/8px wide.
+			while(Serial.available() == 0) {}
+			d1 = Serial.read();
+			sprintf(buff, "CPU:%d%%", d1);
+			display.print(buff);
+		}
+		if(mode == 'Z') {
+			display.display();
+		}
 	}
 }
