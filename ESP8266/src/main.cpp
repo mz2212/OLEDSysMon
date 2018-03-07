9f013b5e1bdceb12907860ed5543361b83717535
Mike <michaelpulliam1@gmail.com>
2018-03-07 16:30:03 -0700

    Actually implement some of the new features
    BigTime! Day of the Week!

--- ESP8266/src/main.cpp
+++ ESP8266/src/main.cpp
@@ -44,115 +44,144 @@ char mode = 1;
 
 void setup()
 {
-    Serial.begin(115200);
+	Serial.begin(115200);
 
-    pinMode(2, INPUT_PULLUP);
-    pinMode(13, INPUT_PULLUP);
-    pinMode(5, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(2), centerButton, FALLING);
-    attachInterrupt(digitalPinToInterrupt(13), topButton, FALLING);
-    attachInterrupt(digitalPinToInterrupt(5), bottomButton, FALLING);
+	pinMode(2, INPUT_PULLUP);
+	pinMode(13, INPUT_PULLUP);
+	pinMode(5, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(2), centerButton, FALLING);
+	attachInterrupt(digitalPinToInterrupt(13), topButton, FALLING);
+	attachInterrupt(digitalPinToInterrupt(5), bottomButton, FALLING);
 
-    u8g2.begin();
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_logisoso16_tf);
+	u8g2.begin();
+	u8g2.clearBuffer();
+	u8g2.setFont(u8g2_font_logisoso16_tf);
 
-    WiFi.begin(ssid, pass);
-    while(WiFi.status() != WL_CONNECTED) {
-        u8g2.clearBuffer();
-        u8g2.drawXBM(32, 14, 64, 64, wifi);
-        u8g2.sendBuffer();
-        delay(100);
-    }
-    url = "/data/2.5/weather?id="+city+"&appid="+appId+"&units="+units;
-    timeClient.begin();
-    os_timer_setfn(&myTimer, update, NULL);
-    os_timer_arm(&myTimer, 900000, true);
-    update(NULL);
+	WiFi.begin(ssid, pass);
+	while(WiFi.status() != WL_CONNECTED) {
+		u8g2.clearBuffer();
+		u8g2.drawXBM(32, 14, 64, 64, wifi);
+		u8g2.sendBuffer();
+		delay(100);
+	}
+	url = "/data/2.5/weather?id="+city+"&appid="+appId+"&units="+units;
+	timeClient.begin();
+	os_timer_setfn(&myTimer, update, NULL);
+	os_timer_arm(&myTimer, 900000, true);
+	update(NULL);
 }
 
 void loop() {
-    timeClient.update();
-    u8g2.clearBuffer();
-    if(display) {
-        switch(mode) {
-            case 1:
-                currTime = timeClient.getFormattedTime();
-                strcpy(textBuff, currTime.c_str());
-                u8g2.drawStr(0, 16, textBuff);
-                #ifdef RSSI
-                sprintf(textBuff, "%d%s", WiFi.RSSI(), "dB");
-                u8g2.drawStr(78, 16, textBuff); // I didn't like this much
-                #endif
-                sprintf(textBuff, "%.1f%s:%s", temp, "C", convertCond(cond).c_str());
-                u8g2.drawStr(0, 32, textBuff);
-                break;
-            case 2:
-                u8g2.drawStr(0, 16, "Placeholder");
-                break;
-            default:
-                break;
-        }
-    }
-    if(pressed || toneState) {
-        toggleTone();
-    }
-    u8g2.sendBuffer();
-    delay(250);
+	timeClient.update();
+	u8g2.clearBuffer();
+	u8g2.setFont(u8g2_font_logisoso16_tf);
+	if(display) {
+		switch(mode) {
+			case 1:
+				currTime = timeClient.getFormattedTime();
+				strcpy(textBuff, currTime.c_str());
+				u8g2.drawStr(0, 16, textBuff);
+				#ifdef RSSI
+				sprintf(textBuff, "%d%s", WiFi.RSSI(), "dB");
+				u8g2.drawStr(78, 16, textBuff); // I didn't like this much
+				#endif
+				sprintf(textBuff, "%.1f%s:%s", temp, "C", convertCond(cond).c_str());
+				u8g2.drawStr(0, 32, textBuff);
+				break;
+			case 2:
+				switch(timeClient.getDay()) {
+					case 0:
+						u8g2.drawStr(0, 16, "Sunday");
+						break;
+					case 1:
+						u8g2.drawStr(0, 16, "Monday");
+						break;
+					case 2:
+						u8g2.drawStr(0, 16, "Tuesday");
+						break;
+					case 3:
+						u8g2.drawStr(0, 16, "Wednesday");
+						break;
+					case 4:
+						u8g2.drawStr(0, 16, "Thursday");
+						break;
+					case 5:
+						u8g2.drawStr(0, 16, "Friday");
+						break;
+					case 6:
+						u8g2.drawStr(0, 16, "Saturday");
+						break;
+					default:
+						u8g2.drawStr(0, 16, "ERROR!");
+						break;
+				}
+				u8g2.setFont(u8g2_font_logisoso32_tn);
+				sprintf(textBuff, "%d%s%d", timeClient.getHours(), ":", timeClient.getMinutes());
+				u8g2.drawStr(0, 64, textBuff);
+				break;
+			default:
+				break;
+		}
+	}
+	if(pressed || toneState) {
+		toggleTone();
+	}
+	u8g2.sendBuffer();
+	delay(250);
 }
 
 void update(void *pArg) {
-    doWeather();
+	doWeather();
 }
 
 void doWeather() {
-    HTTPClient http;
-    http.begin("http://api.openweathermap.org"+url);
-    http.GET();
-    String payload = http.getString();
+	HTTPClient http;
+	http.begin("http://api.openweathermap.org"+url);
+	http.GET();
+	String payload = http.getString();
 
-    JsonObject& root = jsonBuffer.parseObject(payload);
-    temp = root["main"]["temp"];
-    yield();
-    cond = convertCond(root["weather"][0]["icon"].as<char*>());
-    yield();
+	JsonObject& root = jsonBuffer.parseObject(payload);
+	temp = root["main"]["temp"];
+	yield();
+	cond = convertCond(root["weather"][0]["icon"].as<char*>());
+	yield();
 
-    http.end();
+	http.end();
 }
 
 String convertCond(String i) {
-    if(i == "01d" || i == "01n" || i == "CLEAR"){ u8g2.drawXBM(114, 16, 13, 13, sunny); return "CLEAR"; }
-    if(i == "02d" || i == "02n" || i == "OVCST"){ u8g2.drawXBM(114, 16, 13, 8, fewClouds); return "OVCST"; }
-    if(i == "03d" || i == "03n" || i == "04d" || i == "04n" || i == "CLDY"){ u8g2.drawXBM(114, 16, 13, 5, clouds); return "CLDY"; }
-    if(i == "09d" || i == "09n" || i == "RAIN"){ u8g2.drawXBM(114, 16, 13, 10, rain); return "RAIN"; }
-    if(i == "11d" || i == "11n" || i == "TSTRM"){ u8g2.drawXBM(114, 16, 13, 10, tstorm); return "TSTRM"; }
-    if(i == "13d" || i == "13n" || i == "SNOW"){ u8g2.drawXBM(114, 16, 9, 9, snow); return "SNOW"; }
-    if(i == "50d" || i == "FOG"){ u8g2.drawXBM(114, 16, 14, 12, fog); return "FOG"; }
-    return "?";
+	if(i == "01d" || i == "01n" || i == "CLEAR"){ u8g2.drawXBM(114, 16, 13, 13, sunny); return "CLEAR"; }
+	if(i == "02d" || i == "02n" || i == "OVCST"){ u8g2.drawXBM(114, 16, 13, 8, fewClouds); return "OVCST"; }
+	if(i == "03d" || i == "03n" || i == "04d" || i == "04n" || i == "CLDY"){ u8g2.drawXBM(114, 16, 13, 5, clouds); return "CLDY"; }
+	if(i == "09d" || i == "09n" || i == "RAIN"){ u8g2.drawXBM(114, 16, 13, 10, rain); return "RAIN"; }
+	if(i == "11d" || i == "11n" || i == "TSTRM"){ u8g2.drawXBM(114, 16, 13, 10, tstorm); return "TSTRM"; }
+	if(i == "13d" || i == "13n" || i == "SNOW"){ u8g2.drawXBM(114, 16, 9, 9, snow); return "SNOW"; }
+	if(i == "50d" || i == "FOG"){ u8g2.drawXBM(114, 16, 14, 12, fog); return "FOG"; }
+	return "?";
 }
 
 void topButton() {
-    if(mode >= 2) {
-        mode = 1;
-    } else {
-        mode++;
-    }
+	if(mode >= 2) {
+		mode = 1;
+	} else {
+		mode++;
+	}
 }
 
 void centerButton() {
-    pressed = !pressed;
+	pressed = !pressed;
 }
 
 void bottomButton() {
-    display = !display;
+	display = !display;
 }
 
 void toggleTone() {
-    if(toneState) {
-        toneState = !toneState;
-        noTone(15);
-    } else {
-        toneState = !toneState;
-        tone(15, 440);
-    }
+	if(toneState) {
+		toneState = !toneState;
+		noTone(15);
+	} else {
+		toneState = !toneState;
+		tone(15, 440);
+	}
 }
